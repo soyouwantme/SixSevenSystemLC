1794b5dc7fd9ebcf005e36bbd6cbb7a2dc0be413
Kevin <kevinjiang9834@outlook.com>
2019-05-13 00:04:55 +0800

    5.12 完成顶部嵌套的TabLayout，清除了顶部栏

---
+++ app/src/main/java/com/kk/sixsevensystemlc/RestockOrderFragment.java
@@ -0,0 +1,18 @@
+package com.kk.sixsevensystemlc;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RestockOrderFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.restock_order_fragment, container, false);
+    }
+}
