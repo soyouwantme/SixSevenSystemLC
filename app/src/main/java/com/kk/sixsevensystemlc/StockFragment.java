440ab75e192960170a9675b55de786b032c400e4
Kevin <kevinjiang9834@outlook.com>
2019-05-20 17:43:21 +0800

    add stock_message

--- app/src/main/java/com/kk/sixsevensystemlc/StockFragment.java
+++ app/src/main/java/com/kk/sixsevensystemlc/StockFragment.java
@@ -3,17 +3,30 @@ package com.kk.sixsevensystemlc;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class StockFragment extends Fragment {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private StockFragmentPageAdapter stockFragmentPageAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.stock_fragment, container, false);
+
+        viewPager=(ViewPager) view.findViewById(R.id.view_pager);
+        stockFragmentPageAdapter = new StockFragmentPageAdapter(getChildFragmentManager());
+        viewPager.setAdapter(stockFragmentPageAdapter);
+
+        tabLayout= (TabLayout) view.findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
         return view;
     }
 }
---
+++ app/src/main/java/com/kk/sixsevensystemlc/StockItemFragment.java
@@ -0,0 +1,14 @@
+package com.kk.sixsevensystemlc;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class StockItemFragment extends Fragment {
+
+}
